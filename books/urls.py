c7425da9d7e544bb08eeb3e9c193574b0b8dc41e
xasan <muhammadovich.09@gmail.com>
2026-09-16 10:12:16 +0500

    Add book update and delete views

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, BookAddSerializer, BookAddView
+from .views import BookListView, BookDetailView,  BookAddView, BookDeleteUpdateView
 
 urlpatterns = [
     path('', BookListView.as_view()),
-    path('<uuid:pk>/', BookDetailView.as_view()),
-    path('add/', BookAddView.as_view()),
+    path('<uuid:pk>/detail/', BookDetailView.as_view()),
 
+    #permission only admin
+    path('add/', BookAddView.as_view()),
+    path('<uuid:pk>/', BookDeleteUpdateView.as_view()),
 ]
--- books/views.py
+++ books/views.py
@@ -7,6 +7,8 @@ from books.models import Book
 from books.seriaizers import BookListSerializer, BookDetailSerializer, BookAddSerializer
 from shared.custom_pagination import CustomPagination
 
+
+#permission only admin
 class BookAddView(generics.CreateAPIView):
     serializer_class = BookAddSerializer
     permission_classes = [IsAdminUser]
@@ -24,6 +26,36 @@ class BookAddView(generics.CreateAPIView):
             },
             status=status.HTTP_201_CREATED
         )
+
+class BookDeleteUpdateView(generics.DestroyAPIView):
+    serializer_class = BookListSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+    def patch(self, request, *args, **kwargs):
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            data={
+                'success': True,
+                'message': 'Book updated successfully',
+            }
+        )
+
+    def delete(self, request, *args, **kwargs):
+        book = self.get_object()
+        book.delete()
+        return Response(
+            data={
+                'success': True,
+                'message': 'Book deleted successfully',
+            }
+        )
+
 
 class BookListView(generics.ListAPIView):
     serializer_class = BookListSerializer
